fce646e563074aaa90cfefeceac42a149088f5ff
fredminjafred <fminja87@gmail.com>
2023-04-04 14:44:22 +0300

    declaration api

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,8 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/login', function () {
+    $response = ['message' => 'Unauthorized please login again'];
+    return response()->json($response,401);
+})->name('login');
