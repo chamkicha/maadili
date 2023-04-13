753f732a0c8366b2b9edcf28ac26d87a05c4ce10
fredminjafred <fminja87@gmail.com>
2023-04-13 08:15:55 +0300

    declaration api

--- app/Models/Family_member.php
+++ app/Models/Family_member.php
@@ -10,6 +10,19 @@ class Family_member extends Model
 {
     use HasFactory;
 
+    protected $fillable = [
+        'id',
+        'secure_token',
+        'user_id',
+        'family_member_type_id',
+        'sex_id',
+        'first_name',
+        'middle_name',
+        'last_name',
+        'date_of_birth',
+        'occupation'
+    ];
+
     public function member_type(): BelongsTo
     {
 
