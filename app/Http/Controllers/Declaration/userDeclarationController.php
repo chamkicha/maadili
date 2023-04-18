6df3782568f32e9302fcd0c1847c720825ec8aa1
fredminjafred <fminja87@gmail.com>
2023-04-18 17:51:26 +0300

    declaration api

--- app/Http/Controllers/Declaration/userDeclarationController.php
+++ app/Http/Controllers/Declaration/userDeclarationController.php
@@ -169,7 +169,7 @@ class userDeclarationController extends Controller
             if (count($section['section']['data']) > 0) {
 
 
-                DB::table($section['section']['table'])->Insert(
+                DB::table(strtolower($section['section']['table']))->Insert(
                     json_decode(json_encode( $section['section']['data']),true)
                 );
             }
