0d892c1943744607f138c562cb1c400798b67002
chamkicha <jchamkicha@gmail.com>
2024-02-02 06:59:22 +0000

    CH02JAN0959

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Models\Sectiontaarafa478;
+use App\Models\User;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,6 +16,27 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/test', function () {
+
+    
+
+    // $viongozi = User::where('phone_number', '255764242421')->first();
+    // $taarifaAjiras = Sectiontaarafa478::where('user_id',$viongozi->id)->get();
+    $viongozi = User::where('file_number', null)->get();
+    $Ajira = [];
+    foreach($viongozi as $kiongozi){
+        $taarifaAjiras = Sectiontaarafa478::where('user_id',$kiongozi->id)->get();
+        if(!$taarifaAjiras->isEmpty()){
+            $Ajira[]=$kiongozi->id;
+    dd($taarifaAjiras);
+        }
+    }
+
+    dd($Ajira);
+
     return view('welcome');
 });
 
